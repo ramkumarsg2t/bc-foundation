cd7be934df5597b9ec86f7c34aceb08e6b395f46
Ramkumar <ramkumar@g2tsolutions.com>
2015-04-05 23:45:00 +0530

    added core files

---
+++ Scripts/footScripts.js
@@ -0,0 +1,11 @@
+//@codekit-prepend "bower_components/foundation/js/vendor/jquery.js";
+//@codekit-prepend "bower_components/foundation/js/vendor/jquery.cookie.js";
+//@codekit-prepend "bower_components/foundation/js/vendor/fastclick.js";
+//@codekit-prepend "bower_components/foundation/js/vendor/placeholder.js";
+//@codekit-prepend "bower_components/responsive-tables/responsive-tables.js";
+//@codekit-prepend "bower_components/slick-carousel/slick/slick.min.js";
+//@codekit-prepend "bcpieSDK.js";
+//@codekit-prepend "min/devinabox.min.js";
+//@codekit-prepend "dev-bcpie.js";
+//@codekit-prepend "bower_components/foundation/js/foundation.js";
+$(document).foundation();
